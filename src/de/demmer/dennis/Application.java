a379191e85bb16b229fde9cbf8cb6dc47f39871f
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2021-12-14 11:27:15 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,9 +1,14 @@
 package de.demmer.dennis;
 
+
 public class Application {
 
 	public static void main(String[] args) {
 
+		Mail mail = new Mail("Dennis.Demmer@web.de", "Dennis.Demmer@uni-koeln.de");
+		
+		System.out.println(mail.toString());
+	
 	}
 
 }
